b6e4ae426eb39260196b79398a2488c3279e5dca
hugonicolas <hugoncls@gmail.com>
2017-05-06 18:43:41 +0200

    Separation header.php en 2 fichier et creation du dashboard. Ajout d'un dossier dashboard dans views. Version dev page objets.

---
+++ application/view/dashboard/sidebar.php
@@ -0,0 +1,11 @@
+<div class="sidebar">
+	<h3>EggHome</h3>
+	<nav class="sidebar-menu">
+		<p>Menu</p>
+		<ul>
+			<li>Objets</li>
+			<li>Pièces</li>
+			<li>Paramètres</li>
+		</ul>
+	</nav>
+</div>
