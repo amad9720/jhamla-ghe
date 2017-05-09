a1a5a54a0bc60f91edd2870f32cdd5c24f2b8494
Amadou Ly <amly212782@gmail.com>
2017-05-10 01:11:38 +0200

    rebasing the includes

--- application/controller/dashboard.php
+++ application/controller/dashboard.php
@@ -3,16 +3,19 @@
 class Dashboard extends Controller {
 	public function index(){
         require APP . 'view/_templates/head.php';
+        require APP . 'view/dashboard/sidebar.php';
         require APP . 'view/dashboard/objets.php';
     }
 
     public function objets(){
     	require APP . 'view/_templates/head.php';
+        require APP . 'view/dashboard/sidebar.php';
         require APP . 'view/dashboard/objets.php';
     }
 
     public function pieces(){
     	require APP . 'view/_templates/head.php';
+        require APP . 'view/dashboard/sidebar.php';
     	require APP . 'view/dashboard/pieces.php';
     }
 }
