6b702df4a1230d60aed929bd2d8834b8fe6e24cf
hugonicolas <hugoncls@gmail.com>
2017-05-09 09:12:02 +0200

    Ajout controlleur dashboard

---
+++ application/controller/dashboard.php
@@ -0,0 +1,20 @@
+<?php
+
+class Dashboard extends Controller {
+	public function index(){
+        require APP . 'view/_templates/head.php';
+        require APP . 'view/dashboard/objets.php';
+    }
+
+    public function objets(){
+    	require APP . 'view/_templates/head.php';
+        require APP . 'view/dashboard/objets.php';
+    }
+
+    public function pieces(){
+    	require APP . 'view/_templates/head.php';
+    	require APP . 'view/dashboard/pieces.php';
+    }
+}
+
+?>
