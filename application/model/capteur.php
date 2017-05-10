5d324630b7dc89abc0bdc681a29b65a17ee2960d
Amadou Ly <amly212782@gmail.com>
2017-05-10 22:55:47 +0200

    Adding methods to manage capteur and piece

--- application/model/capteur.php
+++ application/model/capteur.php
@@ -7,7 +7,10 @@ class Capteur extends Db_object
     public $id_piece;
     public $id_type;
 
-    //add others attributes for the JOIN
+    // add others attributes for the JOIN
+    public $type;
+    public $date;
+    public $valeur;
 
     protected static $db_table = "capteur"; 
     protected static $db_table_fields = array("id", "etat", "id_piece", "id_type");
@@ -19,6 +22,54 @@ class Capteur extends Db_object
     function __construct()
     {
         
+    }
+
+    public function find_type_capteur() {
+        global $database;
+
+        $sql = "SELECT t.type
+                FROM type_capteurs t
+                WHERE t.id = '{$this->id_type}'
+                LIMIT 1 ";
+
+        // $result contains an array of objects which has properties the columns fetched from the BD by the previous query
+        $result = self::find_by_query($sql);
+
+        return $result->type;
+    }
+
+    public function find_donnee() {
+        global $database;
+
+        $sql = "SELECT d.date, d.valeur
+                FROM donnee d
+                WHERE d.id_capteur = '{$this->id}'
+                LIMIT 1 ";
+
+        // $result contains an array of objects which has properties the columns fetched from the BD by the previous query
+        $result = self::find_by_query($sql);
+
+        return $result;
+    }
+
+    public function add_capteur_to_room($id_piece) {
+        if ($id_piece) {
+            $this->id_piece = $id_piece;
+            return $this->update();
+        }
+        return false;
+    }
+
+    public function add_new_capteur() {
+        if (empty($this->id))
+            return $this->create();
+        else return false;
+    }
+
+    public function remove_capteur() {
+        if (!empty($this->id))
+            return $this->delete();
+        return false;
     }
 
 }
--- application/model/piece.php
+++ application/model/piece.php
@@ -2,10 +2,10 @@
 
 class Piece extends Db_object
 {
+
     public $id;
     public $nom;
     public $id_client;
- 
 
     protected static $db_table = "piece"; 
     protected static $db_table_fields = array("id", "nom", "id_client");
@@ -19,19 +19,31 @@ class Piece extends Db_object
         
     }
 
-    
     public function add_room() {
-        if (!empty($this->nom) /*&& !empty($this->id_client)*/ )
+        if (empty($this->id) /*&& !empty($this->id_client)*/ )
             return $this->create() ? true : false;
         else
-            return false; 
+            return false;
     }
 
     public function remove_room() {
-        if($this->id)
-            return $this->delete() ? true : false;
+        if(!empty($this->id))
+            return $this->delete();
         else
             return false;
+    }
+
+    public function get_room_capteurs() {
+        global $database;
+
+        $sql = "SELECT *
+                FROM capteur c
+                WHERE c.id_piece = '{$this->id}' ";
+
+        // $result contains an array of objects which has properties the columns fetched from the BD by the previous query
+        $result = self::find_by_query($sql);
+
+        return $result;
     }
 
 }
