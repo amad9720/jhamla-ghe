e4987315c89a6bd40e07880b164157dc25a86b9d
azenor-lq <azenor.le-quinio@isep.fr>
2017-05-15 10:07:07 +0200

    Update mission.php

--- application/model/mission.php
+++ application/model/mission.php
@@ -20,7 +20,24 @@ class Mission extends Db_object
      */
     function __construct()
     {
-        
+       
     }
+    
+    public function fetch_missions($id_technicien){
+        $sql = 
+        $missions_en_cours = instanciate('SELECT * FROM mission WHERE date_fin = NULL AND id_technicien = $id_technicien');
+        $missions_finies = instanciate(SELECT * FROM mission WHERE date_fin != NULL AND id_technicien = $id_technicien);
+    }
+    
+    public function formulaire(){
+        /*return an array with different information : date_debut,motif,nom_client, prenom_client*/
+    }
+    
+    public function add_missions($id_technicien){
+        $info = formulaire();
+        
+        $sql = INSERT INTO mission(id,date_debut,date_fin,etat,motif,id_technicien,id_client) VALUES ('',
+        $database->query($sql);
+        
 
 }
