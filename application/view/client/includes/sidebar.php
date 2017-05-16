7e7ed65812dcc3debb2faef27d12ae10755b5fbf
hugonicolas <hugoncls@gmail.com>
2017-05-16 10:14:52 +0200

    Ajout 'Gestion de ma Maison'

--- application/view/client/includes/sidebar.php
+++ application/view/client/includes/sidebar.php
@@ -3,8 +3,9 @@
 	<nav class="sidebar-menu">
 		<p>Menu</p>
 		<ul>
-			<li><a href="<?php echo URL; ?>dashboard/objets/">Objets</a></li>
-			<li><a href="<?php echo URL; ?>dashboard/pieces/">Pièces</a></li>
+			<li><a href="<?php echo URL;?>client/ma_maison">Ma maison</a></li>
+			<li><a href="<?php echo URL; ?>client/objets/">Objets</a></li>
+			<li><a href="<?php echo URL; ?>client/pieces/">Pièces</a></li>
 			<li>Paramètres</li>
 		</ul>
 	</nav>
--- application/view/client/ma_maison.php
+++ application/view/client/ma_maison.php
@@ -1,43 +1,50 @@
 <div class="dashboard-content">
-	<h3>Gestion des pièces</h3>
-	<p class="spacer-small">Cette page vous permet d'ajouter / retirer / editer des pièces présent dans votre domicile</p>
-	<p class="spacer-small">Pour voir la liste des objets présents dans une pièce veuillez éditer cette dernière.</p>
-	<h4 class="spacer-large">Ajouter une pièce</h4>
-	<form class="form spacer-small">
-		<input type="text" placeholder="Nom pièce"/><br/>
-		<input type="submit" name="">
-	</form>
-	<h4 class="spacer-large">Liste des pièces</h4>
-	<table class="table spacer-large">
-		<tr class="first">
-			<th>Nom</th>
-			<th>Nombre objets</th>
-			<th>Editer</th>
-			<th>Supprimer</th>
-		</tr>
-		<tr>
-			<th>Chambre 1</th>
-			<th>5</th>
-			<th>X</th>
-			<th>X</th>
-		</tr>
-		<tr>
-			<th>Salon</th>
-			<th>9</th>
-			<th>X</th>
-			<th>X</th>
-		</tr>
-		<tr>
-			<th>Chambre 2</th>
-			<th>3</th>
-			<th>X</th>
-			<th>X</th>
-		</tr>	
-		<tr>
-			<th>Salle de bain</th>
-			<th>2</th>
-			<th>X</th>
-			<th>X</th>
-		</tr>					
-	</table>
+<h3>Gestion de ma Maison</h3>
+<div class="spacer-small"></div>
+<p>Cette page vous permet de gérer votre domicile</p>
+<div class="spacer-large"></div>
+<h3>Le salon</h3>
+<div class="spacer-small"></div>
+<div class="medium card">
+	<h5>Temperature</h5>
+	<div class="spacer-small"></div>
+	<div class="card-content">
+		<p class="card-main-info">20°</p>
+		<p class="card-bottom-text">11:21 AM - Lille, France</p>
+	</div>
+	</div>
+<div class="medium card">
+	<h5>Humidité</h5>
+	<div class="spacer-small"></div>
+	<div class="card-content">
+		<p class="card-main-info">60%</p>
+		<p class="card-bottom-text">11:21 AM - Lille, France</p>
+	</div>
+	</div>
+<div class="medium card">
+	<h5>Consomation</h5>
+	<div class="spacer-small"></div>
+	<div class="card-content">
+		<p class="card-main-info">12kW/h</p>
+		<p class="card-bottom-text">11:21 AM - Lille, France</p>
+	</div>
+	</div>	
+<div class="medium card">
+	<h5>Lampe</h5>
+	<div class="spacer-small"></div>
+	<div class="card-content">
+		<p class="card-main-info green-text">ON</p>
+		<p class="card-bottom-text">11:21 AM - Lille, France</p>
+	</div>
+	</div>
+<div class="medium card">
+	<h5>Lampe</h5>
+	<div class="spacer-small"></div>
+	<div class="card-content">
+		<p class="card-main-info green-text">ON</p>
+		<p class="card-bottom-text">11:21 AM - Lille, France</p>
+	</div>
+	</div>	
+</div>
+</div>
 </div>
