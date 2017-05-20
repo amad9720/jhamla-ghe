1c8defba064ec733ec4cac1556a9623fddf4f1a5
marcevrd <evrard.marc96@gmail.com>
2017-05-20 13:32:21 +0200

    page offre

--- application/view/invite/offres.php
+++ application/view/invite/offres.php
@@ -1,24 +1,16 @@
-<!DOCTYPE html>
-<html>
-	<head>
-		<meta charset="utf-8" />
-		<link rel="stylesheet" href="style.css" />
-		<title>OFFRES</title>
-	</head>
-	<body>
-		<p> offre 1</p>
-		<p> offre 2</p>
-		<p> offre 3</p>
-		<p> offre 4</p>
+<div classe="container">
 
 		<div id="block">
-			<div class="offre">offre 1</div>
-			<div class="offre">offre 2</div>
-			<div class="offre">offre 3</div>
+			<div class="offre">
+				<h4>offre classique </h4>
+				</div>
+			<div class="offre"> 
+				<h4>offre medium </h4>
+				</div>
+			<div class="offre"><h4>offre prenium </h4>
+				</div>
 		</div>
 
-
-	</body>
+</div>
 	
-	<footer></footer>
 	
