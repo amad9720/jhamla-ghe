251932a9c7ef2d1876f5208e6b74e9cc269622fb
marcevrd <evrard.marc96@gmail.com>
2017-05-23 10:18:21 +0200

    offre php début

--- application/model/offre.php
+++ application/model/offre.php
@@ -20,4 +20,30 @@ class Offre extends Db_object
         
     }
 
+    public static function get_offres(){
+        $offres = Offre::find_all();
+
+        return $offres;
+
+    }
+    
+    
+    public static function get_offre($id_offre){
+        $offre = Offre::find_by_id($id_offre);
+
+        return array_unshift($offre);
+    }
+
+    public function update_offre(){
+        //verif l'existance de l'offre 
+        if ($this->id){
+            $this->update();
+        }
+        
+
+
+
+    }
+
+
 }
